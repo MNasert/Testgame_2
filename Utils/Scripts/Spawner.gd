63df71430fd355756e184b9e917d84e7e1d9efec
Maximilian Nasert <maximilian.nasert@gmx.de>
2023-08-28 20:40:40 +0200

    adwws

--- Utils/Scripts/Spawner.gd
+++ Utils/Scripts/Spawner.gd
@@ -5,12 +5,30 @@ extends Node2D
 @export var timeout_seconds: float
 
 @onready var waveTimer = $Wavetimer
+@onready var spawnTimer = $SpawnTimeout
 
 var level
-# Called when the node enters the scene tree for the first time.
+var enemyQueue: Array[Node2D] = []
+
 func _ready():
 	self.level = get_tree().get_root()
+	self.waveTimer.start(self.timeout_seconds)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func generate_entities(type: PackedScene, num: int):
+	for i in range(num):
+		var e = type.instantiate()
+		e.init(self.pos)
+		self.enemyQueue.append(e)
+	
+func _on_wavetimer_timeout():
+	for enemyType in Enemies:
+		for num_to_spawn in numEnemiesPerType:
+			self.generate_entities(enemyType, num_to_spawn)
+	self.spawnTimer.start(.1)
+	self.waveTimer.start(self.timeout_seconds)
+	
+func _on_spawn_timeout_timeout():
+	if len(self.enemyQueue) > 0:
+		level.add_child(self.enemyQueue.pop_front())
+	if len(self.enemyQueue) > 0:
+		self.spawnTimer.start(.1)
