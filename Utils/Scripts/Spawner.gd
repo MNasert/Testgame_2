b85294e21a995623695abab821cf0a416bdf9119
Maximilian Nasert <maximilian.nasert@gmx.de>
2023-08-31 00:35:03 +0200

    dasdasd

--- Utils/Scripts/Spawner.gd
+++ Utils/Scripts/Spawner.gd
@@ -12,12 +12,12 @@ var enemyQueue: Array[Node2D] = []
 
 func _ready():
 	self.level = get_tree().get_root()
-	self.waveTimer.start(self.timeout_seconds)
+	self.waveTimer.start(1)
 
 func generate_entities(type: PackedScene, num: int):
 	for i in range(num):
 		var e = type.instantiate()
-		e.init(self.pos)
+		e.init(self.position)
 		self.enemyQueue.append(e)
 	
 func _on_wavetimer_timeout():
@@ -31,4 +31,4 @@ func _on_spawn_timeout_timeout():
 	if len(self.enemyQueue) > 0:
 		level.add_child(self.enemyQueue.pop_front())
 	if len(self.enemyQueue) > 0:
-		self.spawnTimer.start(.1)
+		self.spawnTimer.start(1)
